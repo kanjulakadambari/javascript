aad2332c9777b4ca896a8aee5f3480a01c37e17f
kadambari <kanjulakadambari69@gmail.com>
2026-04-14 09:46:56 +0530

    kada

---
+++ script.js
@@ -0,0 +1,76 @@
+//const users = [
+   // {
+
+    //id: 1,
+    //name: "user1",
+    //loc: "hyd"
+    //},
+    //{
+        //id: 2,
+        //name: "user2",
+        //loc: "hyder"
+    //},
+
+//]
+
+//console.log(users[1].loc);
+
+/*function myFun(a , b) {
+    console.log(a+b);
+    //return a+b;
+}
+//console.log(myFun);
+console.log(myFun(10 , 20));*/
+
+
+
+/*const myFun = function(num1 , num2) {
+    return num1+num2;
+}
+console.log(myFun(10,30));*/
+
+/*function domath( num1, num2, operation) {
+    switch(operation){
+        case "*":
+        return num1*num2;
+        case "+":
+        return num1+num2;
+        case "/":
+        return num1/num2;
+        case "-":
+        return num1-num2;
+
+    }
+    console.log(domath(10,20 ,"+"));
+
+}*/
+
+
+/*let name = "kadambari";
+let age = "22";
+
+console.log("my name is"+ name+". she is "+ age);//type 1
+console.log(`my name is ${name}. she is ${age}`);//type 2*/
+
+/*let buchi = document.getElementById("buchi");
+
+buchi.innerText = "<b>hello</b>";
+buchi.innerHTML = "<b>hello</b>";*/
+
+
+let boxes = document.getElementsByClassName("box");
+let para = document.getElementsByClassName("para");
+
+// it will print box 1, box 2 ,box 3//
+
+boxes[1].innerHTML ="KADA";
+
+// it will print box 1, KADA, box 3//
+
+para[0].innerHTML ="hey";
+
+
+
+
+
+
