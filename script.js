946c179c550771e9c4482a819604ea09200bfd77
kadambari <kanjulakadambari69@gmail.com>
2026-04-15 09:50:25 +0530

    events

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ buchi.innerText = "<b>hello</b>";
 buchi.innerHTML = "<b>hello</b>";*/
 
 
-let boxes = document.getElementsByClassName("box");
+/*let boxes = document.getElementsByClassName("box");
 let para = document.getElementsByClassName("para");
 
 // it will print box 1, box 2 ,box 3//
@@ -67,7 +67,44 @@ boxes[1].innerHTML ="KADA";
 
 // it will print box 1, KADA, box 3//
 
-para[0].innerHTML ="hey";
+para[0].innerHTML ="hey";*/
+
+// Events
+
+/*const myFun = () => {
+    console.log("hello");
+};*/
+
+
+/*const myFun = () => {
+    let display = document.getElementById("display")
+    display.innerHTML = "bye" // this change to hello to bye only one time
+    display.innerHTML = display.innerHTML === "hello" ? "bye" : "hello" //this change many times hello to bye
+
+
+    if(display.innerHTML === "hello") {
+    display.style.color = "darkblue"
+    }
+    else {
+        display.style.color = "red"
+    }
+
+}*/
+
+let isShow = true
+const myFun = () => {
+
+    let display = document.getElementById("display")
+
+    if(isShow) {
+        display.src = "https://wallup.net/wp-content/uploads/2016/01/186050-lion-animals.jpg"
+        isShow = !isShow
+    }
+    else {
+        display.src = "https://tse4.mm.bing.net/th/id/OIP.ubwBIfs0I_XeI62cmaBONAHaE8?pid=Api&P=0&h=180"
+        isShow = !isShow
+    }
+}
 
 
 
